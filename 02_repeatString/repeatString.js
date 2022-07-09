97c00408c42e2f055f2202216a4ba26cac066b04
Hunter Duger <hduger1@gmail.com>
2022-07-09 01:18:46 -0400

    Passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = ""
+    if (num < 0) {
+        return "ERROR";
+    }
+    if (string === "") {
+        return ''
+    }
+    for (i = 0; i < num; i++) {
+        word += string;
+    
+        
+    }
+    return word;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// const repeatString = function(string, num) {
+//     let word = ""
+//     for (i = 0; i < num; i++) {
+//         word += string;
+        
+//     }
+//     return word;
+// };
