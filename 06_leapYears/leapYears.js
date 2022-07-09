b84ffe631f36514360a9b6630f67a1e6d5c6c8a0
Hunter Duger <hduger1@gmail.com>
2022-07-09 18:02:38 -0400

    Wrote code to pass last three tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(year) {
-    if (year%4 !== 0) return false;
+    if ((year%100 ===0 && year%400 !==0) || year%4 !== 0) return false;
     if (year%4 === 0) return true;
 
 };
