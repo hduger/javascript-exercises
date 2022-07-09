4e9b633bc2949d62090e854ccdcddc924f57823a
Hunter Duger <hduger1@gmail.com>
2022-07-09 01:31:56 -0400

    Wrote 4 lines that passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let split = string.split("");
+    let revArray = split.reverse();
+    let revString = revArray.join("");
+    return revString;
+    //fill array with string
+    //reverse array
+    //print reverse array as string
 
 };
 
