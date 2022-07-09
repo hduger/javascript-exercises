4f72bd82e9ef7ce1862ab8c391fb40d0431c78b9
Hunter Duger <hduger1@gmail.com>
2022-07-09 18:24:06 -0400

    Wrote code that passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempFar) {
+  let convertToCels = (tempFar-32)*(5/9);
+  convertToCels = Math.round(convertToCels*10)/10;
+  return convertToCels;
 };
 
-const ctof = function() {
+const ctof = function(tempCels) {
+  let convertToFar = (tempCels*(9/5))+32;
+  convertToFar = Math.round(convertToFar *10)/10;
+  return convertToFar;
 
 };
 
