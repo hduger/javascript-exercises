364ca7a048b5fa4efcc730a674e211989815b99b
Hunter Duger <hduger1@gmail.com>
2022-07-15 20:50:28 -0400

    finised tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
-};
+const sum = function(array) {
 
-const multiply = function() {
+	return array.reduce((a, b)=> a + b, 0);
 
 };
 
-const power = function() {
-	
+const multiply = function(array) {
+  return array.length ? array.reduce((a, b) => a*b) : 0;
+
 };
 
-const factorial = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
+
+const factorial = function(num) {
+	if (num === 0) return 1;
+  prod = 1;
+  for (let i = num; i >=1; i--) {
+    prod *= i;
+  }
+  return prod; 
+  }
 
 // Do not edit below this line
 module.exports = {
