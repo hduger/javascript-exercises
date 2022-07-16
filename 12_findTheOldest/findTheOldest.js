ba623622feb7419398ff759a282f817df95a2d0d
Hunter Duger <hduger1@gmail.com>
2022-07-16 19:11:08 -0400

    passed tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.sort((a, b)=> a.yearOfBirth > b.yearOfBirth ? 1:-1);
+    
 
+    return oldest;
 };
 
 // Do not edit below this line
